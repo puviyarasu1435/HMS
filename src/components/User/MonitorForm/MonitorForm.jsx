176f82584c899b97d6e13558be3b35f66312f1ad
PUVIYARASU P <59604648+puviyarasu1435@users.noreply.github.com>
2025-03-26 14:36:49 +0530

    Update MonitorForm.jsx

--- src/components/User/MonitorForm/MonitorForm.jsx
+++ src/components/User/MonitorForm/MonitorForm.jsx
@@ -15,7 +15,7 @@ import { useSelector } from "react-redux";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080");
+const socket = io("https://hms-api-ibja.onrender.com");
 
 const validationSchema = Yup.object({
   HeartRate: Yup.number()
