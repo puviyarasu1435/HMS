06a51167d14540a27ebc23c1761cfeb51406eeba
PUVIYARASU P <59604648+puviyarasu1435@users.noreply.github.com>
2025-03-26 14:32:27 +0530

    Update User.Api.js

--- src/Redux/API/User.Api.js
+++ src/Redux/API/User.Api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const UserApi = createApi({
   reducerPath: "UserApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://hms-api-ibja.onrender.com" }),
   tagTypes: ["user","test"],
   endpoints: (builder) => ({
     fetchUsers: builder.query({
