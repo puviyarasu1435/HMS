4406dbdb6532e099cda9f61edd029dd673d9dcc1
PUVIYARASU P <59604648+puviyarasu1435@users.noreply.github.com>
2025-03-26 15:40:14 +0530

    Update Prediction.Api.js

--- src/Redux/API/Prediction.Api.js
+++ src/Redux/API/Prediction.Api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const PredictionApi = createApi({
   reducerPath: "PredictionApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://56e2-2405-201-e039-f062-e8fd-2121-5619-9dc.ngrok-free.app" }),
   endpoints: (builder) => ({
     predict: builder.mutation({
       query: (data) => ({
