b7e0dad66b72d63a1591a2432386ea4c34eff27e
PUVIYARASU P <59604648+puviyarasu1435@users.noreply.github.com>
2025-03-26 14:32:54 +0530

    Update Prediction.Api.js

--- src/Redux/API/Prediction.Api.js
+++ src/Redux/API/Prediction.Api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const PredictionApi = createApi({
   reducerPath: "PredictionApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://hms-api-ibja.onrender.com" }),
   endpoints: (builder) => ({
     predict: builder.mutation({
       query: (data) => ({
